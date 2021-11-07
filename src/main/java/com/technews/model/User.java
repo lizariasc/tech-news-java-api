16e1e9ad0c144e3ba82ee0a73fc93ddf85527894
Liz A <lizethariasc14@gmail.com>
2021-11-07 12:59:36 -0800

    comments

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+// persistable object, so that the User class can map to a table
 @Entity
+// specifies properties that should be ignored when serializing this object to JSON
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+// specifies the name of the table that this class maps to
 @Table(name = "user")
 public class User {
 

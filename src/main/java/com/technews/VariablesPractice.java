746ce056395191b836ca596afd1fca92fc8211a0
Liz A <lizethariasc14@gmail.com>
2021-11-07 10:47:56 -0800

    VariablesPractice class

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,21 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int whole = 2;
+        double num = 2.5;
+        String a = "loved";
+
+        // sum variable is a double
+        double sum = whole + num;
+
+        // love variable is a String
+        String love = a + whole;
+
+        // test sum result
+        System.out.println(sum);
+
+        // text love result
+        System.out.println(love);
+       }
+}

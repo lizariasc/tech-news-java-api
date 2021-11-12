ece4b69dada7724d2740e7b45bdcf0ca7a4cf78d
Liz A <lizethariasc14@gmail.com>
2021-11-12 05:26:58 -0800

    class-level annotations

--- src/main/java/com/technews/model/Vote.java
+++ src/main/java/com/technews/model/Vote.java
@@ -1,5 +1,12 @@
 package com.technews.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "vote")
+public class Vote {}
+
 public class Vote {
     private Integer id;
     private Integer userId;

bcbf882b09beafd34649b5c97df2e69f65d3ed69
Liz A <lizethariasc14@gmail.com>
2021-11-12 05:24:09 -0800

    @id

--- src/main/java/com/technews/model/Comment.java
+++ src/main/java/com/technews/model/Comment.java
@@ -2,11 +2,16 @@ package com.technews.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "comment")
 
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String commentText;
     private Integer userId;

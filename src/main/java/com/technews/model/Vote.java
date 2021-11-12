5c7e35c18c2db6b91d5e5e2d9ab59702782c6408
Liz A <lizethariasc14@gmail.com>
2021-11-12 05:27:36 -0800

    @id

--- src/main/java/com/technews/model/Vote.java
+++ src/main/java/com/technews/model/Vote.java
@@ -2,12 +2,17 @@ package com.technews.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "vote")
 public class Vote {}
 
 public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private Integer userId;
     private Integer postId;
